ee1d14456a7240e708ac332c7eec858c34bd3cc8
Ekin AbaLıoğlu <abaliogluekin@gmail.com>
2021-10-29 00:35:04 +0300

    Delete unnecessary import

--- backend/src/routes/cms/index.js
+++ backend/src/routes/cms/index.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const mongoose = require('mongoose');
 const router = express.Router();
 const ContentType = require('../../models/content-type');
 const Content = require('../../models/content');
