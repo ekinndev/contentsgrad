4aa71e58b283d2ee29c32ff71e9ada709ebe60a0
Ekin AbaLıoğlu <abaliogluekin@gmail.com>
2021-10-28 22:09:03 +0300

    define main routes and error handlers

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,12 +1,23 @@
 const express = require('express');
 const app = express();
-const exampleRoute = require('./routes/');
+
+const cmsAppRoutes = require('./routes/cms');
+const webAppRoutes = require('./routes/web');
+
 require('./mongo-connection');
 
-app.use('/ekin', exampleRoute);
+app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+app.use('/cms', cmsAppRoutes);
+app.use('/web', webAppRoutes);
+
+app.get('*', (req, res) => {
+    res.send('404');
+});
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
 });
 
 module.exports = app;
