31fd7f37e774314abca532b3f61271bbbfce568d
Ekin AbaLıoğlu <abaliogluekin@gmail.com>
2021-10-30 14:58:35 +0300

    Add example test

--- backend/src/__tests__/index.js
+++ backend/src/__tests__/index.js
@@ -1,6 +1,14 @@
+const mongoose = require('mongoose');
+const app = require('../app');
+const request = require('supertest')(app);
+
 const sum = (a, b) => {
     return a + b;
 };
+
+afterAll(async () => {
+    await mongoose.connection.close();
+});
 
 test('3 plus 4 must be equal 7', () => {
     const result = sum(3, 4);
@@ -12,4 +20,9 @@ test('3 plus 5 must not be equal 7', () => {
     const result = sum(3, 6);
 
     expect(result).not.toBe(7);
+});
+
+test('Will this work?', async () => {
+    const response = await request.get('/web/me/');
+    expect(response.text).toBe('hello world this is Ekin');
 });
