95ea8dc9a49ab6f0280ad45bd30ec4f55a15ff41
Ekin Abalıoğlu <abaliogluekin@gmail.com>
2021-11-03 23:30:24 +0300

    Add documentation for authentication endpoints

--- backend/src/routes/cms/user.js
+++ backend/src/routes/cms/user.js
@@ -4,6 +4,47 @@ const User = require('../../models/user');
 const Validator = require('async-validator').default;
 const router = express.Router();
 
+/**
+ * @swagger
+ * /user/register/:
+ *   post:
+ *     tags: [User]
+ *     summary: Register an user
+ *     responses:
+ *       200:
+ *         description: Create user and to start session.
+ *         content:
+ *           application/json:
+ *             schema:
+ *                type: String
+ *                example: OK
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                 type: string
+ *                 description: User's email.
+ *                 example: hello@gmail.com
+ *               password:
+ *                 type: string
+ *                 description: User's password
+ *                 example: 123456
+ *               passwordConfirmation:
+ *                 type: string
+ *                 description: Confirmation password
+ *                 example: 123456
+ *               name:
+ *                 type: string
+ *                 description: User's name
+ *                 example: Ekin
+ *
+ *
+ */
+
 router.post('/register', async (req, res, next) => {
     const descriptor = {
         name: [
@@ -55,6 +96,86 @@ router.post('/register', async (req, res, next) => {
     res.sendStatus(200);
 });
 
+/**
+ * @swagger
+ * /user/session/:
+ *   post:
+ *     tags: [User]
+ *     summary: Logged in an existing user
+ *     responses:
+ *       200:
+ *         description: Logged in an existing user and to start session
+ *         content:
+ *           application/json:
+ *             schema:
+ *                type: object
+ *                properties:
+ *                   _id:
+ *                      type: string
+ *                      example: 6182c206874844d37934bfcb
+ *                   name:
+ *                      type: string
+ *                      example: ekin
+ *                   email:
+ *                      type: string
+ *                      example: hello@test.com
+ *                   salt:
+ *                      type: string
+ *                      example: 72b78e625f60415bbee928c0e6273e2152c161c0ba1cb64fdedf4a227f85ec88
+ *                   hash:
+ *                      type: string
+ *                   createdAt:
+ *                      type: string
+ *                   updatedAt:
+ *                      type: string
+ *       401:
+ *         description: The credentials does not match from db
+ *         content:
+ *           application/json:
+ *             schema:
+ *                type: object
+ *                properties:
+ *                  message:
+ *                      type: string
+ *                      example: The username and password you provided did not match our records. Please double-check and try again.
+ *                  status:
+ *                      type: integer
+ *                      example: 401
+ *       400:
+ *         description: The user already logged in
+ *         content:
+ *           application/json:
+ *             schema:
+ *                type: object
+ *                properties:
+ *                  message:
+ *                      type: string
+ *                      example: User is already logged in.
+ *                  status:
+ *                      type: integer
+ *                      example: 400
+ *
+ *
+ *
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                 type: string
+ *                 description: User's email.
+ *                 example: hello@test.com
+ *               password:
+ *                 type: string
+ *                 description: User's password
+ *                 example: hellofromotherside
+ *
+ *
+ */
+
 router.post(
     '/session',
     passport.authenticate('local', { failWithError: true }),
@@ -73,6 +194,24 @@ router.post(
         });
     },
 );
+
+/**
+ * @swagger
+ * /user/logout/:
+ *   delete:
+ *     tags: [User]
+ *     summary: To logout and destroying the logged in user
+ *     responses:
+ *       200:
+ *         description: To logout and destroying the logged in user
+ *         content:
+ *           application/json:
+ *             schema:
+ *                type: String
+ *                example: OK
+ *
+ *
+ */
 
 router.get('/logout', function (req, res, next) {
     req.logout();
