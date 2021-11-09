94d4583f426d60813b4c9fe1b7d38bb78d180805
Ekin Abalıoğlu <abaliogluekin@gmail.com>
2021-11-09 17:26:07 +0300

    Get user profile if user already logged in and has valid session

--- frontend/src/store/account.js
+++ frontend/src/store/account.js
@@ -12,16 +12,27 @@ const accountModule = {
     },
     actions: {
         async login({ commit, dispatch }, data) {
-            const user = await cmsApi.post('/user/session', data);
-            await dispatch('content/getSpaces', null, { root: true });
-            await dispatch('content/getLanguages', null, { root: true });
-            await dispatch('content/getContentTypes', null, { root: true });
-            commit('setUser', user.data);
+            try {
+                const user = await cmsApi.post('/user/session', data);
+                commit('setUser', user.data);
+            } catch (e) {
+                if (e.response.status == 400) {
+                    await dispatch('getProfile');
+                }
+            } finally {
+                await dispatch('content/getSpaces', null, { root: true });
+                await dispatch('content/getLanguages', null, { root: true });
+                await dispatch('content/getContentTypes', null, { root: true });
+            }
         },
         async logout({ commit }) {
             await cmsApi.delete('/user/logout');
             commit('setUser', null);
             commit('content/setContentTypes', null, { root: true });
+        },
+        async getProfile({ commit }) {
+            const user = await cmsApi.get('/user/profile');
+            commit('setUser', user.data);
         },
     },
     getters: {},
