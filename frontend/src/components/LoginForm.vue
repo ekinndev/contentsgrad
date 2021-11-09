8a475dfecc86dce39b96e8057c992a31f6c43fbd
Ekin Abalıoğlu <abaliogluekin@gmail.com>
2021-11-09 15:24:44 +0300

    Create login page

---
+++ frontend/src/components/LoginForm.vue
@@ -0,0 +1,47 @@
+<template>
+    <a-row align="middle" justify="center">
+        <a-col :span="24">
+            <a-form
+                :model="form"
+                :label-col="{ span: 3 }"
+                :wrapper-col="{ span: 19 }"
+                layout="vertical"
+                class="loginForm"
+            >
+                <a-form-item label="Email">
+                    <a-input v-model="form.email" placeholder="Email"></a-input>
+                </a-form-item>
+                <a-form-item label="Password">
+                    <a-input v-model="form.password" type="password" placeholder="123456"></a-input>
+                </a-form-item>
+                <a-form-item :wrapper-col="{ span: 12, offset: 11 }">
+                    <a-button type="primary" @click="handleSubmit">Log in</a-button>
+                </a-form-item>
+            </a-form>
+        </a-col>
+    </a-row>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            form: {
+                email: '',
+                password: '',
+            },
+        };
+    },
+    methods: {
+        handleSubmit() {
+            this.$emit('login', this.form);
+        },
+    },
+};
+</script>
+
+<style scoped lang="scss">
+.loginForm {
+    width: 800px;
+}
+</style>
---
+++ frontend/src/views/Login.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="login">
+        <login-form @login="submit" />
+    </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import LoginForm from '@/components/LoginForm';
+import { mapActions, mapState } from 'vuex';
+
+export default {
+    name: 'LoginPage',
+    components: {
+        LoginForm,
+    },
+    computed: {
+        ...mapState('account', ['user']),
+    },
+    methods: {
+        ...mapActions('account', ['login']),
+        async submit(values) {
+            await this.login(values);
+            this.$router.push('/');
+        },
+    },
+};
+</script>
