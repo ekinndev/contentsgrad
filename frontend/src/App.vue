e1eb9cc0ec79e1932ccab71207a430a1ee5d7d55
Ekin Abalıoğlu <abaliogluekin@gmail.com>
2021-11-09 15:23:50 +0300

    Add Layout component and wrap whole app

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,32 +1,17 @@
 <template>
     <div id="app">
-        <div id="nav">
-            <router-link to="/">{{ $t('home') }}</router-link> |
-            <router-link to="/about">{{ $t('about') }}</router-link>
-        </div>
-        <router-view />
+        <layout>
+            <router-view />
+        </layout>
     </div>
 </template>
 
-<style lang="scss">
-#app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-}
+<script>
+import Layout from './components/Layout.vue';
 
-#nav {
-    padding: 30px;
-
-    a {
-        font-weight: bold;
-        color: #2c3e50;
-
-        &.router-link-exact-active {
-            color: #42b983;
-        }
-    }
-}
-</style>
+export default {
+    components: {
+        Layout,
+    },
+};
+</script>
---
+++ frontend/src/components/Layout.vue
@@ -0,0 +1,96 @@
+<template>
+    <a-layout class="layoutMinHeight">
+        <a-layout-sider theme="light" v-model="collapsed" collapsible>
+            <a-menu :default-selected-keys="['1']" mode="inline">
+                <a-menu-item v-for="menuItem in getMenuItems" :key="menuItem.key">
+                    <a-icon :type="menuItem.icon" /><span>
+                        <router-link v-if="menuItem.path" :to="menuItem.path" exact> {{ menuItem.title }} </router-link>
+                        <a-button type="link " v-else @click="menuItem.func"> {{ menuItem.title }} </a-button>
+                    </span>
+                </a-menu-item>
+            </a-menu>
+        </a-layout-sider>
+        <a-layout>
+            <a-layout-content class="layoutContent">
+                <a-row type="flex" justify="center" align="middle" class="centeredRow">
+                    <a-col>
+                        <slot />
+                    </a-col>
+                </a-row>
+            </a-layout-content>
+        </a-layout>
+    </a-layout>
+</template>
+<script>
+import { mapState, mapActions } from 'vuex';
+
+export default {
+    name: 'Layout',
+
+    data() {
+        return {
+            collapsed: false,
+        };
+    },
+    methods: {
+        ...mapActions('account', ['logout']),
+    },
+    computed: {
+        ...mapState('account', ['contentTypes']),
+        getMenuItems() {
+            if (this.contentTypes?.length > 0) {
+                return [
+                    {
+                        key: '1',
+                        icon: 'home',
+                        title: 'Home',
+                        path: '/',
+                    },
+                    ...this.contentTypes?.map?.(contentType => ({
+                        key: contentType._id,
+                        title: contentType.name,
+                        icon: 'file-text',
+                        path: `/content-types/${contentType._id}`,
+                    })),
+                    {
+                        key: '2',
+                        icon: 'logout',
+                        title: 'Log out',
+                        path: null,
+                        func: async () => {
+                            await this.logout();
+                            this.$router.replace('/login');
+                        },
+                    },
+                ];
+            }
+            return [
+                {
+                    key: '1',
+                    icon: 'setting',
+                    title: 'Settings',
+                    path: '/settings',
+                },
+                {
+                    key: '3',
+                    icon: 'login',
+                    title: 'Log in',
+                    path: '/login',
+                },
+            ];
+        },
+    },
+};
+</script>
+<style lang="scss" scoped>
+.layoutContent {
+    margin: 0 16px;
+}
+.layoutMinHeight {
+    min-height: 100vh;
+}
+.centeredRow {
+    align-items: center;
+    height: 100%;
+}
+</style>
