cf678526e35ede7b3187aa6b1925ff974189b305
Ekin Abalıoğlu <abaliogluekin@gmail.com>
2021-11-09 15:22:10 +0300

    Add ant design components and css

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,11 +1,15 @@
 import Vue from 'vue';
 import App from './App.vue';
+import Antd from 'ant-design-vue';
+import 'ant-design-vue/dist/antd.css';
 import './registerServiceWorker';
 import router from './router';
 import store from './store';
 import i18n from './i18n';
 
 Vue.config.productionTip = false;
+
+Vue.use(Antd);
 
 new Vue({
     router,
