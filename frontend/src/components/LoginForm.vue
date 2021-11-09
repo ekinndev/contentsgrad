b57b31f9257f72fb5787f22430dff09e7ef9debf
Ekin Abalıoğlu <abaliogluekin@gmail.com>
2021-11-09 22:42:11 +0300

    Add login form validation

--- frontend/src/components/LoginForm.vue
+++ frontend/src/components/LoginForm.vue
@@ -1,47 +1,62 @@
 <template>
-    <a-row align="middle" justify="center">
-        <a-col :span="24">
-            <a-form
-                :model="form"
-                :label-col="{ span: 3 }"
-                :wrapper-col="{ span: 19 }"
-                layout="vertical"
-                class="loginForm"
-            >
-                <a-form-item label="Email">
-                    <a-input v-model="form.email" placeholder="Email"></a-input>
-                </a-form-item>
-                <a-form-item label="Password">
-                    <a-input v-model="form.password" type="password" placeholder="123456"></a-input>
-                </a-form-item>
-                <a-form-item :wrapper-col="{ span: 12, offset: 11 }">
-                    <a-button type="primary" @click="handleSubmit">Log in</a-button>
-                </a-form-item>
-            </a-form>
+    <a-row type="flex" justify="center">
+        <a-col :span="12">
+            <box title="Login into ContentsGrad">
+                <a-form :form="form" :label-col="{ span: 4 }" :wrapper-col="{ span: 14 }" @submit="handleSubmit">
+                    <a-form-item label="Email">
+                        <a-input
+                            v-decorator="[
+                                'email',
+                                { rules: [{ required: true, message: 'Please input your email!' }] },
+                            ]"
+                            placeholder="Email"
+                        ></a-input>
+                    </a-form-item>
+                    <a-form-item label="Password">
+                        <a-input
+                            v-decorator="[
+                                'password',
+                                {
+                                    rules: [
+                                        { required: true, message: 'Please enter your password!' },
+                                        { min: 6, message: 'Please enter a valid password!' },
+                                    ],
+                                },
+                            ]"
+                            type="password"
+                            placeholder="123456"
+                        ></a-input>
+                    </a-form-item>
+                    <a-form-item :wrapper-col="{ span: 12, offset: 4 }">
+                        <a-button type="primary" htmlType="submit" @click="handleSubmit">Log in</a-button>
+                    </a-form-item>
+                </a-form>
+            </box>
         </a-col>
     </a-row>
 </template>
 
 <script>
+import Box from '@/components/Box';
 export default {
+    components: {
+        Box,
+    },
     data() {
         return {
-            form: {
-                email: '',
-                password: '',
-            },
+            form: this.$form.createForm(this, { name: 'login' }),
         };
     },
     methods: {
-        handleSubmit() {
-            this.$emit('login', this.form);
+        handleSubmit(e) {
+            e.preventDefault();
+
+            this.form.validateFields((err, values) => {
+                if (!err) {
+                    this.$emit('login', values);
+                }
+            });
         },
     },
 };
 </script>
-
-<style scoped lang="scss">
-.loginForm {
-    width: 800px;
-}
-</style>
