412fa428761ca3a4b33ad93c9a7898b6a61a027a
Ekin Abalıoğlu <abaliogluekin@gmail.com>
2021-11-09 22:41:18 +0300

    Add box component

---
+++ frontend/src/components/Box.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="box">
+        <h3>{{ title }}</h3>
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        title: {
+            type: String,
+            default: 'ContentsGrad',
+        },
+    },
+};
+</script>
+
+<style scoped lang="scss">
+.box {
+    margin-top: 3rem;
+    background: #fff;
+    padding: 3rem;
+}
+</style>
