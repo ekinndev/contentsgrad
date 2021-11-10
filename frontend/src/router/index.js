60523df12ab1742a2ccd15fee3d32c55aa8a1bdf
Ekin Abalıoğlu <abaliogluekin@gmail.com>
2021-11-10 23:15:07 +0300

    Add route for spaces

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -35,6 +35,14 @@ const routes = [
         component: () => import(/* webpackChunkName: "about" */ '../views/settings/ContentTypes.vue'),
     },
     {
+        path: '/settings/spaces',
+        name: 'SSpaces',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () => import(/* webpackChunkName: "about" */ '../views/settings/Spaces.vue'),
+    },
+    {
         path: '/content-types/:id',
         component: () => import(/* webpackChunkName: "content-type" */ '../views/ContentTypes.vue'),
     },
