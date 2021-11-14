82e53f96059f0f2055cae85821cdef8ac6b62a47
Ekin Abalıoğlu <abaliogluekin@gmail.com>
2021-11-14 14:01:12 +0300

    Add delete content type feature

--- frontend/src/store/content.js
+++ frontend/src/store/content.js
@@ -56,6 +56,10 @@ const contentModule = {
             await cmsApi.post(`/content-types`, data);
             await dispatch('getContentTypes');
         },
+        async deleteContentType({ dispatch }, data) {
+            await cmsApi.delete(`/content-types/${data}`);
+            await dispatch('getContentTypes');
+        },
         async deleteLanguage({ dispatch }, data) {
             await cmsApi.delete(`/languages/${data}`);
             await dispatch('getLanguages');
