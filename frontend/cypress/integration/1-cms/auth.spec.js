4d9dfea513945efdc49e5113cb3e1a4174699545
Ekin Abalıoğlu <abaliogluekin@gmail.com>
2022-03-15 17:31:53 +0000

    Add login and logout tests

--- frontend/cypress/integration/1-cms/auth.spec.js
+++ frontend/cypress/integration/1-cms/auth.spec.js
@@ -1,8 +1,15 @@
-describe('It should login successfully', () => {
-    before(() => {
+describe('Authentication', () => {
+    it('It should be login successfully', () => {
         cy.visit('http://localhost:8080');
         cy.get('#login_email').type('hello@test.com');
         cy.get('#login_password').type('hellofromotherside');
         cy.get('.ant-btn').click();
+
+        cy.get('body').contains('Homepage');
+    });
+
+    it('It should be logout successfully', () => {
+        cy.contains('Log out').click();
+        cy.get('body').not('Homepage');
     });
 });
